92e8986135191006e2a559c6cc7ab768f0e43431
huz <inexplicable.huiqing@gmail.com>
2013-09-26 01:27:25 -0700

    extracted assertOld to utils and added unit test for it

--- test/utils-test.js
+++ test/utils-test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var should = require('should'),
-	utils = require('../lib/utils.js'),
+	utils = require('../lib/utils'),
 	path = require('path'),
 	when = require('when'),
 	fs = require('graceful-fs'),
@@ -139,6 +139,93 @@ describe('utils', function(){
 			done();
 		});
 	});
+    
+    describe('#assertOld', function(){
+        
+        it('should use the heuristic to determine when gc is hurting tps', function(done){
+        
+            //whenever tps grow up, whether or not other metrics goes up/down assertion should be false.
+            var pid = Math.floor(process.pid * (1 + Math.random())),
+                assertOld = utils.assertOld;
+            
+            _.each(_.range(0, 1000), function(ith){
+            
+                assertOld({
+                    'pid': pid,
+                    'tps': ith,
+                    'cpu': ith,
+                    'memory': ith,
+                    'gc': {
+                        'incremental': ith,
+                        'full': ith
+                    }
+                }).should.equal(false);
+            });
+            
+            done();
+        });
+        
+        it('should assert true whenever a degradation of more than 10% happens', function(done){
+        
+            var pid = Math.floor(process.pid * (1 + Math.random())),
+                assertOld = utils.assertOld;
+            
+            assertOld({
+                'pid': pid,
+                'tps': 50,
+                'cpu': 50,
+                'memory': 1000000000,
+                'gc': {
+                    'incremental': 100,
+                    'full': 10
+                }
+            }).should.equal(false);
+            
+            debugger;
+            assertOld({
+                'pid': pid,
+                'tps': 40,//over 10%
+                'cpu': 55,//higher
+                'memory': 1100000000,
+                'gc': {
+                    'incremental': 110,
+                    'full': 11
+                }
+            }).should.equal(true);
+            
+            done();
+        });
+        
+        it('should give 10% margin for tolerance', function(done){
+            
+            var pid = Math.floor(process.pid * (1 + Math.random())),
+                assertOld = utils.assertOld;
+            
+            assertOld({
+                'pid': pid,
+                'tps': 50,
+                'cpu': 50,
+                'memory': 1000000000,
+                'gc': {
+                    'incremental': 100,
+                    'full': 10
+                }
+            }).should.equal(false);
+            
+            assertOld({
+                'pid': pid,
+                'tps': 48,//less than 10%
+                'cpu': 55,//higher
+                'memory': 1100000000,
+                'gc': {
+                    'incremental': 110,
+                    'full': 11
+                }
+            }).should.equal(false);
+            
+            done();
+        });
+    });
 
 	after(function(done){
 
