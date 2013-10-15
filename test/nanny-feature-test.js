c5972689a6c23052f468ac43b19d2345bae79309
zhuwang <zhuwang@ebay.com>
2013-10-15 11:13:55 -0700

    Used deathQueue to kill the unresponsive worker and create a new successor Changed the name convention Added test case and modified test/lib/server.js to support the test case

---
+++ test/nanny-feature-test.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var should = require('should'),
+    request = require('request'),
+    _ = require('underscore'),
+    spawn = require('child_process').spawn,
+    EventEmitter = require('events').EventEmitter,
+    emitter = new EventEmitter();
+
+describe('Test Cluster2 Nanny Feature', function () {
+
+    var childProc;
+
+    before(function (done) {
+        this.timeout(10000);
+        var env = {};
+        _.extend(env, process.env);
+        _.extend(env, {
+            host: '127.0.0.1',
+            port: 3000,
+            noWorkers: 2,
+            heartbeatInterval: 500,
+            maxHeartbeatDelay: 1500
+        });
+
+        childProc = spawn('node', ['test/lib/server.js'], {
+            env: env,
+            stdio: ['pipe', 1, 2, 'ipc']
+        });
+
+        childProc.once('message', function (msg) {
+            if (msg.ready) {
+                return done();
+            }
+        });
+        //setTimeout(done, 5000);
+    });
+
+    it('Should get the run away worker id and get notified when the worker died', function (done) {
+        var pid;
+        this.timeout(10000);
+        request.get('http://127.0.0.1:3000/nanny-feature-test', function (err, res, body) {
+            should.not.exist(err);
+            body.should.be.ok;
+            pid = parseInt(body);
+            //console.log('body: ' + pid);
+        });
+        childProc.on('message', function (msg) {
+            if (msg.dead) {
+                msg.pid.should.equal(pid);
+                return done();
+            }
+        });
+    });
+    
+    after(function (done) {
+        childProc.kill('SIGKILL');
+        done();
+    });
+});
