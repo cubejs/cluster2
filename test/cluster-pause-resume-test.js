0bb645ab2de7092a48369f19be6b58de9ed1fadb
huzhou <huzhou@ebay.com>
2013-12-17 14:14:10 -0800

    updated dep using snapshot versions, fixed broken unit test

--- test/cluster-pause-resume-test.js
+++ test/cluster-pause-resume-test.js
@@ -1,71 +1,81 @@
 'use strict';
 
-var request = require('request');
-var should = require('should');
-var fork = require('child_process').fork;
-var utils = require('../lib/utils');
+var request = require('request'),
+    should = require('should'),
+    fork = require('child_process').fork,
+    utils = require('../lib/utils');
 
-describe('Test Pause and Resume the Worker', function () {
+describe('Test Pause and Resume the Worker', function (){
     
-    var childProc;
-    var port;
+    var childProc,
+        port;
     
-    before(function (done) {
+    before(function(done){
+        
+        this.timeout(5000);
+
         var token = 't-' + Date.now();
-        utils.pickAvailablePorts(9090, 9190, 2).then(function (ports) {
-            port = ports[0]
-            childProc = fork(require.resolve('./lib/cluster-pause-resume-runtime.js'), ['--token=' + token], {env: {port: ports[0], monPort: ports[1]}});
-            childProc.once('message', function (msg) {
-                if (msg.ready) {
-                    return done();
-                }else if (msg.err) {
-                    console.log(msg.err);
-                    return done(msg.err);
-                }
+        
+        utils.pickAvailablePorts(9090, 9190, 2).then(function(ports){
+                
+                port = ports[0];
+                
+                childProc = fork(require.resolve('./lib/cluster-pause-resume-runtime.js'), ['--token=' + token], {
+                    'env': {'port': port, 'monPort': ports[1]}
+                });
+
+                childProc.once('message', function (msg){
+                    
+                    done(msg.err);
+                });
+            },
+            function (err) { 
+
+                done(err);
             });
-        }).otherwise(function (err) { 
-            return done(err);
-        });
     });
 
-    after(function (done) {
+    after(function (done){
+
         childProc.kill('SIGTERM');
         done();
     });
 
-    it('Should pause the worker', function (done) {
-        this.timeout(5000);
-        childProc.send({operation: 'pause'});
+    it('Should pause the worker', function (done){
 
-        childProc.once('message', function (msg) {
+        this.timeout(5000);
+        
+        childProc.send({'operation': 'pause'});
+
+        childProc.on('message', function (msg) {
             if (msg.paused) {
+                console.log('[paused]');
                 request.get({
-                    url: 'http://127.0.0.1:' + port + '/sayHello',
-                    timeout: 4000
-                }, function (err, res, body) {
-                    if (err) {
-                        done();
-                    }else {
-                        done(new Error('Should be error here'));
-                    }
-                });
+                        'url': 'http://127.0.0.1:' + port + '/sayHello',
+                        'timeout': 4000
+                    }, 
+                    function (err, res, body) {
+                        done(err ? undefined : new Error('should have got error here'));
+                    });
             }
         });
     });
 
-    it('should resume the worker', function (done) {
-        this.timeout(5000);
-        childProc.send({operation: 'resume'});
+    it('should resume the worker', function (done){
 
-        childProc.once('message', function (msg) {
+        this.timeout(5000);
+
+        childProc.send({'operation': 'resume'});
+
+        childProc.on('message', function (msg){
             if (msg.resumed) {
-                request.get('http://127.0.0.1:' + port + '/sayHello', function (err, res, body) {
-                    if (err) {
-                        done(err);
-                    }
+                console.log('[resumed]');
+                request.get('http://127.0.0.1:' + port + '/sayHello', function (err, res, body){
+                    
                     res.statusCode.should.equal(200);
                     body.should.equal('hello');
-                    done();
+
+                    done(err);
                 });
             }
         });
--- test/lib/cluster-pause-resume-runtime.js
+++ test/lib/cluster-pause-resume-runtime.js
@@ -1,11 +1,13 @@
 'use strict';
 
-var listen = require('../../index').listen;
-var express = require('express');
-var app = express();
+var listen = require('../../index').listen,
+    express = require('express'),
+    _ = require('underscore'),
+    app = express();
 
 function configureApp() {
-    app.get('/sayHello', function (req, res) {
+
+    app.get('/sayHello', function (req, res){
         res.send('hello', 200);
     });
 
@@ -16,38 +18,58 @@ listen({
     'noWorkers': 1,
     'createServer': require('http').createServer,
     'app': app,
-    'port': parseInt(process.env.port) || 9090,
+    'port': parseInt('' + process.env.port, 10) || 9090,
     'configureApp': configureApp,
     'cache': {
-        'enable': false
+        'enable': true
     },
     'ecv': {
         'mode': 'control',
         'root': '/ecv'
     },
     'monCreateServer': require('http').createServer,
-    'monPort': parseInt(process.env.monPort) || 9091
-}).then(function (resolved) {
-    //console.log(resolved);
-    if (resolved.worker) {
-        return;
-    }
-    var master = resolved.master;
-    var workerPid = Object.keys(master.puppets)[0];
-    process.on('message', function (msg) {
-        if (msg.operation === 'pause') {
-            master.pause(workerPid).then(function (resolved) {
-                process.send({paused: true});
-            });
+    'monPort': parseInt('' + process.env.monPort, 10) || 9091
+})
+.then(function(resolved){
+    
+        if(resolved.worker){
+            return;
         }
-        if (msg.operation === 'resume') {
-            master.resume(workerPid).then(function (resolved) {
-                process.send({resumed: true});
-            });
-        }
+
+        var master = resolved.master,
+            workerPid = _.last(_.keys(master.puppets));
+
+        console.log('[test] pause & resume pid:%d', workerPid);
+
+        process.on('message', function(msg){
+
+            if(msg.operation === 'pause') {
+
+                master.pause(workerPid).then(function(resolved){
+
+                        console.log('[test] paused');
+                        process.send({'paused': true});
+                    }, 
+                    function(err){
+
+                        console.log('[test] error:\n' + err.stack);
+                        process.send({'paused': false});
+                    });
+            }
+            if(msg.operation === 'resume'){
+
+                master.resume(workerPid).then(function(resolved){
+                        process.send({'resumed': true});
+                    }, 
+                    function(err){
+                        process.send({'resumed': false});
+                    });
+            }
+        });
+
+        process.send({'ready': true});
+    },
+    function (err) {
+        
+        process.send({'err': err});
     });
-    process.send({ready: true});
-}).otherwise(function (err) {
-    console.log(err);
-    process.send({err: err});
-});
